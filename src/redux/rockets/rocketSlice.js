6dcc81b16d6754889daab86d54d45faafcb4fcbf
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-05-18 12:10:53 +0500

    Create basic strurture for Rockets

---
+++ src/redux/rockets/rocketSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import React from 'react';
+
+const rocketSlice = createSlice({
+  name: 'rocket',
+  reducers: {
+
+  },
+});
+
+export default rocketSlice;
