cd7ef9c18536ceac52130e42d45c6a1eba43182f
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-05-18 10:42:38 +0500

    Create empty Myprofile

---
+++ src/components/Missions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Missions() {
+  return (
+    <>
+      <h1>My Missions</h1>
+      <h2>Page Under Construction</h2>
+    </>
+  );
+}
+
+export default Missions;
---
+++ src/rountes/MyProfileRoute.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MyProfileView from '../view/MyProfileview';
+
+const ProfileRoute = () => (
+  <MyProfileView />
+);
+
+export default ProfileRoute;
