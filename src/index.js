92834e805af88f43bfc0c5ea10e8defdb80ebfa4
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-05-18 15:07:01 +0500

    Remove linter errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
-import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
